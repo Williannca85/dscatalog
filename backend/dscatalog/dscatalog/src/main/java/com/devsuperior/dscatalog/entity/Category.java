869566a46176037eed32d959d647a6510fda4c4b
Williannca85 <williannca@hotmail.com>
2024-02-22 20:24:39 -0300

    feat: criação da classe Category.java

---
+++ backend/dscatalog/dscatalog/src/main/java/com/devsuperior/dscatalog/entity/Category.java
@@ -0,0 +1,40 @@
+package com.devsuperior.dscatalog.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Data
+
+@AllArgsConstructor
+@Table(name = "tb_category")
+public class Category implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    /**
+     * @Override é para sobre escrever
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof Category category))
+            return false;
+        return Objects.equals(getId(), category.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
